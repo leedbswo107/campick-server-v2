329bde865382913addefaadbacd9ee4acee18845
leedbswo107 <ljyu1@naver.com>
2024-10-21 20:07:43 +0900

    feat: uri encode

--- controller/authController.js
+++ controller/authController.js
@@ -153,9 +153,7 @@ const authorize = async (req, res) => {
   }
   const uri = `https://kauth.kakao.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code${scopeParam}`;
   const encoded = encodeURI(uri);
-  res
-    .status(302)
-    .redirect(encoded);
+  res.status(302).redirect(encoded);
 };
 
 const call = async (method, uri, param, header) => {
